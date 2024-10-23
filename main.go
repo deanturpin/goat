514a54f9e280c8caf517fb13cf6f7a5512b18014
Dean Turpin <defofdean@gmail.com>
2024-10-23 22:41:35 +0100

    Update API endpoint and improve JSON output formatting

--- main.go
+++ main.go
@@ -10,24 +10,25 @@ import (
 
 func main() {
 
-	// Step 1: Make the HTTP GET request
-	resp, err := http.Get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD")
+	// Make the HTTP GET request
+	resp, err := http.Get("https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&limit=3")
 	if err != nil {
 		log.Fatalf("Failed to fetch data: %v", err)
 	}
 	defer resp.Body.Close() // Always close the response body
 
-	// Step 2: Read the response body
-	body, err := ioutil.ReadAll(resp.Body)
-	
+	// Read the response body
+	body, err := ioutil.ReadAll(resp.Body)	
 	if err != nil {
 		log.Fatalf("Failed to read response body: %v", err)
 	}
 
-	fmt.Println(string(body))
-
-	// print price
+	// Pretty print json
 	var result map[string]interface{}
 	json.Unmarshal(body, &result)
-	fmt.Println("Price of BTC in USD: ", result["USD"])
+	prettyJSON, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		log.Fatalf("Failed to generate pretty json: %v", err)
+	}
+	fmt.Println(string(prettyJSON))
 }

1b84a5efb5e0d60d06859be1603903e63bd5d949
Dean Turpin <defofdean@gmail.com>
2024-10-23 22:17:11 +0100

    fetch url

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"io/ioutil"
+	"log"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+
+	// Step 1: Make the HTTP GET request
+	resp, err := http.Get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD")
+	if err != nil {
+		log.Fatalf("Failed to fetch data: %v", err)
+	}
+	defer resp.Body.Close() // Always close the response body
+
+	// Step 2: Read the response body
+	body, err := ioutil.ReadAll(resp.Body)
+	
+	if err != nil {
+		log.Fatalf("Failed to read response body: %v", err)
+	}
+
+	fmt.Println(string(body))
+
+	// print price
+	var result map[string]interface{}
+	json.Unmarshal(body, &result)
+	fmt.Println("Price of BTC in USD: ", result["USD"])
 }
